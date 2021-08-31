db22b7a34b1b17aab1c0c28c23017bc949eba8a4
Joel Majors <joelamajors@gmail.com>
2021-08-31 10:58:57 -0400

    early attempts at batch reporting

--- health_check_report.py
+++ health_check_report.py
@@ -1,7 +1,53 @@
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 import json
+import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime
+# import concurrent
 
+now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+def get_secret():
+    secret_name = "GoogleAnalyticsAPI"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name)
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            raise e
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+
+    secret_json = json.loads(secret)
+
+    with open('client_secret.json', 'w') as client_secret:
+        client_secret.write(json.dumps(secret_json))
+
+get_secret()
+scope = 'https://www.googleapis.com/auth/analytics.readonly'
+key_file_location = './client_secret.json'
+
+webproperties = {}
 
 def get_service(api_name, api_version, scopes, key_file_location):
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
@@ -12,35 +58,51 @@ def get_service(api_name, api_version, scopes, key_file_location):
     return service
 
 
+def list_webproperties(request_id, response, exception):
+    if exception is not None:
+        "aw dang"
+        pass
+    else:
+        webproperties = response
+        pass
+
+
 def get_profile_ids(service):
     # Gets view_ids for individual properties
     accounts = service.management().accounts().list().execute()
-    view_ids = []
+
+    batch = service.new_batch_http_request()
 
     if accounts.get('items'):
         accounts_items = accounts.get('items')
 
         for account_item in accounts_items:
             account_id = account_item.get('id')
+            batch.add(service.management().webproperties().list(accountId=account_id), callback=list_webproperties)
 
-            properties = service.management().webproperties().list(
-                    accountId=account_id).execute()
+    batch.execute()
 
-            if properties.get('items'):
-                property_items = properties.get('items')
+    view_ids = []
 
-                for prop_item in property_items:
-                    prop_id = prop_item.get('id')
+    print(webproperties)
 
-                    profiles = service.management().profiles().list(
-                            accountId=account_id,
-                            webPropertyId=prop_id).execute()
+    if webproperties.get('items'):
+        property_items = webproperties.get('items')
 
-                    if profiles.get('items'):
-                        profile_items = profiles.get('items')
+        for prop_item in property_items:
+            prop_id = prop_item.get('id')
 
-                        for prof_item in profile_items:
-                            view_ids.append(prof_item.get('id'))
+            profiles_batch.add(service.management().profiles().list(
+                    accountId=account_id,
+                    webPropertyId=prop_id).execute())
+
+    profiles = profiles_batch.execute()
+
+    if profiles.get('items'):
+        profile_items = profiles.get('items')
+
+        for prof_item in profile_items:
+            view_ids.append(prof_item.get('id'))
 
     return view_ids
 
@@ -55,10 +117,6 @@ def get_results(service, profile_id):
 
 
 def main():
-    # Define the auth scopes to request.
-    scope = 'https://www.googleapis.com/auth/analytics.readonly'
-    key_file_location = './client_secrets.json'
-
     # Authenticate and construct service.
     service = get_service(
             api_name='analytics',
